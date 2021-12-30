72a95a8bb459e88248319b00eaf59d7cbdc6f791
tshifhiwa <tdmadima@gmail.com>
2021-12-30 20:00:45 +0200

    mvie

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import React,{useState} from  'react' 
+
+
+
 
 function App() {
+ 
+  const [movies, setMovies] = useState([]);
+
+
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Movie rater</h1>
       </header>
+      <div className="layout">
+      <div>Movie List</div>
+      <div>
+        {
+          movies.map(movie => {
+            return <h2>{movie}</h2>
+          })
+        }
+
+      </div>
+      <div>Movie details</div>
+      </div>
     </div>
   );
 }
